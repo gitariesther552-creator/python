fc3d81899c1b8c0453a30d047fb92acddd692183
gitariesther552-creator <gitariesther552@gmail.com>
2026-06-15 14:14:05 +0300

    added python lesson 1a

---
+++ lesson1a.py
@@ -0,0 +1,32 @@
+# This is a comment
+"""
+This is also a comment.
+"""
+'''
+
+This is another comment.
+'''
+
+#what is a variable: variables are labels which acts as containers to hold/store data values
+
+#in python we use assignment operator (=) to assign values to a valuable
+
+message=" Hello there, hope you're doing good"
+print(message)
+
+message2=" i love python programming"
+print(message2)
+
+#concatenation : joining of two or more variables to get an output
+
+message3=message+message2
+print(message3)
+
+_name="Esther"
+print(_name)
+
+total_marks="68"
+print(total_marks)
+
+# for ="john" // this will show an error
+# print(for)
