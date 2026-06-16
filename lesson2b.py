c361b292ffa9dd233c1a0c75be71e682de36444c
gitariesther552-creator <gitariesther552@gmail.com>
2026-06-16 13:44:41 +0300

    lesson two b

--- lesson2b.py
+++ lesson2b.py
@@ -1,3 +1,20 @@
 #Python Tuples
 #A tuple is a type of a list that is not mutable(unchangeable)
-#
+#To introduce atuple, we use the parenthesis/brackets ()
+
+counties = ("Nairobi", "Machakos", "Kisumu", "Kisii", "Narok", "Mombasa", "Lamu", "Kilifi")
+print(counties)
+print(type(counties))
+
+#A tuple stores its items on indexes
+# We also starts from index zero onwards
+print(counties[3])
+
+#Slicing of Tuples
+print(counties[3:])
+
+print(counties[1:6])
+
+#note; below code will show an error because atuple is immutable
+counties.append("Nakuru")
+print(counties)
